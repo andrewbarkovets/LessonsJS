d98777ab1f7c421a13632a48c8d125217d444021
Andrew Barkovets <andrewbarkovets@gmail.com>
2020-10-14 01:40:04 +0300

    fix(main.js): new fix

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
-let money = 900; 
-const income = "freelance";
-const addExpenses = "home, school, car, mobile";
-const deposit = true;
-const mission = 100000;
-const period = 12;
-const budgetDay = (money / 30);
+const money = 900, 
+    income = "freelance",
+    addExpenses = "home, school, car, mobile",
+    deposit = true,
+    mission = 100000,
+    period = 12,
+    budgetDay = (money / 30);
 
-
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+console.log(typeof money, typeof income, typeof deposit);
 
 console.log(addExpenses.length);
 
 console.log("Период равен" + " " + period + " " + "месяцев");
 console.log("Цель заработать" + " " + mission + " " + "долларов");
 
-console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split(","));
+console.log(addExpenses.toLowerCase(), addExpenses.split(","));
 
 console.log(budgetDay);
 
-// alert('Привет Java Script 2020');
-// console.log('Я изучаю Java Script');
+alert('Привет Java Script 2020');
+console.log('Я изучаю Java Script');
