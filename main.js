dc50c23b48a9933307bafe608a0e104f837f67d0
Andrew Barkovets <andrewbarkovets@gmail.com>
2020-10-13 20:17:29 +0300

    feat(main.js): new feat

--- main.js
+++ main.js
@@ -4,6 +4,22 @@ const addExpenses = "home, school, car, mobile";
 const deposit = true;
 const mission = 100000;
 const period = 12;
+const budgetDay = (money / 30);
 
-alert('Привет Java Script 2020');
-console.log('Я изучаю Java Script');
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log("Период равен" + " " + period + " " + "месяцев");
+console.log("Цель заработать" + " " + mission + " " + "долларов");
+
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(","));
+
+console.log(budgetDay);
+
+// alert('Привет Java Script 2020');
+// console.log('Я изучаю Java Script');
