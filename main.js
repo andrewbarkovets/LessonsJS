6cc41ee420c1ca1b0123b087ce57fbd71a7e5750
Andrew Barkovets <andrewbarkovets@gmail.com>
2020-10-13 19:28:55 +0300

    feat(main.js): new feat

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let money, 
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let money = 900; 
+const income = "freelance";
+const addExpenses = "home, school, car, mobile";
+const deposit = true;
+const mission = 100000;
+const period = 12;
 
 alert('Привет Java Script 2020');
 console.log('Я изучаю Java Script');
