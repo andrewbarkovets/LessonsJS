f14ee7ea5cb1aaf4a4d3ce5dda4943c5dbd2d97b
Andrew Barkovets <andrewbarkovets@gmail.com>
2020-10-15 20:17:46 +0300

    fix(main.js): new fix

--- main.js
+++ main.js
@@ -29,13 +29,13 @@ const constantFlow2 = prompt('Введите обязательную стать
 const spentMoney2 = +prompt('Во сколько это обойдется?', '500');
 
 // 1) Функция возвращает сумму всех обязательных расходов за месяц
-const getExpensesMonth = function (spentMoney1, spentMoney2) {
-    if (!spentMoney1) {
-        spentMoney1 = 0;
-    }
-    if (!spentMoney2) {
-        spentMoney2 = 0;
-    }
+const getExpensesMonth = (spentMoney1 = 0, spentMoney2 = 0) => {
+    // if (!spentMoney1) {
+    //     spentMoney1 = 0;
+    // }
+    // if (!spentMoney2) {
+    //     spentMoney2 = 0;
+    // }
 
     return spentMoney1 + spentMoney2;
 };
@@ -77,7 +77,7 @@ const getStatusIncome = () => {
         return ('У вас высокий уровень дохода');
     }   else if (budgetDay <= 1200 && budgetDay >= 600) {
         return ('У вас средний уровень дохода');
-    }   else if (budgetDay <= 600 && budgetDay >= 0) {
+    }   else if (budgetDay < 600 && budgetDay >= 0) {
         return ('К сожалению у вас уровень дохода ниже среднего');
     }   else if (budgetDay < 0) {
         return ('Что то пошло не так');
