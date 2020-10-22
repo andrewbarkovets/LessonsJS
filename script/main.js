8365a5c20767d09029e41d23e456f8323af76b59
Andrew Barkovets <andrewbarkovets@gmail.com>
2020-10-22 19:50:29 +0300

    fix(main.js): newfix

--- script/main.js
+++ script/main.js
@@ -2,25 +2,28 @@
 
 // Переменные ДЗ№9
 const calcStart = document.getElementById('start'),
-    incomeAdd = document.getElementsByTagName('button'),
-    expensesAdd = document.getElementsByTagName('button'),
+    incomeAdd = document.getElementsByTagName('button')[0],
+    expensesAdd = document.getElementsByTagName('button')[1],
+
     depositCheck = document.querySelector('#deposit-check'),
     additionalIncomeItem = document.querySelectorAll('.additional_income-item'),
-    budgetMonthValue = document.querySelector('.budget_month-value'), // в верстке стоит до budget_day-value
-    budgetDayValue = document.getElementsByClassName('budget_day-value'),
-    expensesMonthValue = document.getElementsByClassName('expenses_month-value'),
-    additionalIncomeValue = document.getElementsByClassName('additional_income-value'),
-    additionalExpensesValue = document.getElementsByClassName('additional_expenses-value'),
-    incomePeriodValue = document.getElementsByClassName('income_period-value'),
-    targetMonthValue = document.getElementsByClassName('target_month-value'),
+    budgetMonthValue = document.querySelector('.budget_month-value'),
+    budgetDayValue = document.getElementsByClassName('budget_day-value')[0],
+    expensesMonthValue = document.getElementsByClassName('expenses_month-value')[0],
+    additionalIncomeValue = document.getElementsByClassName('additional_income-value')[0],
+    additionalExpensesValue = document.getElementsByClassName('additional_expenses-value')[0],
+    incomePeriodValue = document.getElementsByClassName('income_period-value')[0],
+    targetMonthValue = document.getElementsByClassName('target_month-value')[0],
     salaryAmount = document.querySelector('.salary-amount'),
-    incomeTitle = document.querySelector('.income-title'),
+    incomeTitle = document.querySelectorAll('.income-title')[1],
     incomeAmount = document.querySelector('.income-amount'),
-    expensesTitle = document.querySelector('.expenses-title'),
+    expensesTitle = document.querySelectorAll('.expenses-title')[1],
     expensesAmount = document.querySelector('.expenses-amount'),
     additionalExpensesItem = document.querySelector('.additional_expenses-item'),
     periodSelect = document.querySelector('.period-select');
 
+console.log(incomeTitle);
+console.log(expensesTitle);
 
 // Функция проверки на число!
 const isNumber = function(n) {
